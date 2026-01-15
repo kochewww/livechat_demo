976f4dd00caf75727a5a22b33d8cb5132b18cef3
Vlad Kochew <vlad.kochew@memco.ru>
2026-01-15 17:03:42 +0700

    fix: correct height for safari

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import { ChatContextProvider } from "@/features/chat/model/ChatContext";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gradient-to-br from-slate-900 via-slate-950 to-slate-900 text-slate-50 flex flex-col md:items-center md:justify-center md:px-4 md:py-4">
-      <div className="w-full h-full md:h-auto md:max-w-2xl md:max-h-[90vh] bg-white/10 border-0 md:border border-white/10 backdrop-blur-lg md:rounded-2xl shadow-2xl overflow-hidden flex flex-col">
+    <div className="min-h-[100svh] bg-gradient-to-br from-slate-900 via-slate-950 to-slate-900 text-slate-50 flex flex-col md:items-center md:justify-center md:px-4 md:py-4">
+      <div className="w-full flex-grow md:h-auto md:max-w-2xl md:max-h-[90vh] bg-white/10 border-0 md:border border-white/10 backdrop-blur-lg md:rounded-2xl shadow-2xl overflow-hidden flex flex-col">
         <ChatContextProvider>
           <ChatHeader />
           <ChatMessages />
